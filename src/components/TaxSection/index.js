7f61b7f5a6b5e463df47dae5eb5ebeb247c8b5fb
Malavan Srikumar <malavan.srikumar@zendesk.com>
2021-12-27 22:20:40 +1100

    Notify user of unpaid users

--- src/components/TaxSection/index.js
+++ src/components/TaxSection/index.js
@@ -8,23 +8,32 @@ import { zipIdToName } from '../../utils/helpers'
 import { fetchUnpaidUsers } from '../../utils/apis/admin'
 import { PALETTE } from '@zendeskgarden/react-theming';
 import { Inline } from '@zendeskgarden/react-loaders';
+import { Notification, Title, Close } from '@zendeskgarden/react-notifications';
+import { OrderedList, UnorderedList } from '@zendeskgarden/react-typography';
+import DefaultBlock from '../DefaultBlock'
+
 
 
 const TaxInfo = () =>(
-  <>
-    <XXL >Tax Collection</XXL>
+  <DefaultBlock sm={14}>
+    <XXL>{"Tax Collection"}</XXL>
     <Paragraph>
-      <MD tag="span"> { "The contribution that is required to be paid by each individual"} </MD>
+      <MD tag="span"> { "The contribution that is required to be paid by each individual."} </MD>
     </Paragraph>
-  </>
+  </DefaultBlock>
 )
 
 const TaxUnpaidNotification = ({ unpaidNames }) => (
-  <div>
-  the following users have not paid {unpaidNames.map(name => <p>{name}</p>)}
- 
 
-  </div>
+  <DefaultBlock sm={14}>
+  <Notification type="warning">
+    <Title>{"Warning"}</Title>
+    <MD tag="span"> {"The tax cannot be calculated since the following users have not paid."} </MD>
+    <UnorderedList>
+      {unpaidNames.map(name => <UnorderedList.Item>{name}</UnorderedList.Item>)}
+    </UnorderedList>
+  </Notification> 
+  </DefaultBlock>
 )
 
 const findUnpaidNames = ( unpaidUsers, allUsers) =>{
@@ -46,19 +55,17 @@ const TaxSection = ({ tax }) => {
     })
   }, [tax, interval])
 
-
-
   return(
-    <>
+    <DefaultBlock sm={12}>
       <TaxInfo/>
-    { tax.length && !isLoading 
+    { !unpaidUsers.length 
       ?
       <UserAmountTable data={tax}/> 
       :
       <TaxUnpaidNotification  unpaidNames={findUnpaidNames(unpaidUsers, allUsers)}/>
     }
     { isLoading && <Inline size={32} color={PALETTE.blue[600]} />}
-    </>
+    </DefaultBlock>
   )
 }
 
