c1ef2aa97edbcc1900a2bf1cb67a657238e8ce30
Malavan Srikumar <srikumar.malavan@gmail.com>
2022-07-02 21:19:22 +1000

    Add multichoice to show how many datapoints to show

--- src/pages/MetricsPage/index.js
+++ src/pages/MetricsPage/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Paragraph } from '@zendeskgarden/react-typography';
-import { MD, XXXL, XXL } from '@zendeskgarden/react-typography';
+import { Field, Label, Radio } from '@zendeskgarden/react-forms';
+import { Row, Col } from '@zendeskgarden/react-grid';import { Paragraph } from '@zendeskgarden/react-typography';
+import { MD, XXXL, XXL, LG } from '@zendeskgarden/react-typography';
 import { PALETTE } from '@zendeskgarden/react-theming';
 import { Inline } from '@zendeskgarden/react-loaders';
 import { ComposedChart, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
@@ -39,7 +40,7 @@ const IncomeAndTaxPerUser = ({userIdToName}) => {
       <div style={{marginBottom:'3%'}}>
         <XXL>{"Total Income Received and Total Tax Contributed by User"}</XXL>
       </div>
-      <ResponsiveContainer width="100%" height="100%">
+      <ResponsiveContainer width="95%" height="100%">
        <BarChart width={500} height={300} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5, }} >
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="Name" />
@@ -62,9 +63,42 @@ const IncomeAndTaxPerUser = ({userIdToName}) => {
   );
 }
 
+const Multichoice = ({radioValue, setRadioValue}) => {
+
+  return (
+    <Row justifyContent="center">
+      <Col size="auto">
+          <Field>
+            <Radio value="10" checked={radioValue === '10'} onChange={event => setRadioValue(event.target.value)} >
+              <Label><LG>Show 10</LG></Label>
+            </Radio>
+          </Field>
+      </Col>
+      <Col size="auto">
+          <Field>
+            <Radio value="20" checked={radioValue === '20'} onChange={event => setRadioValue(event.target.value)} >
+              <Label><LG>Show 20</LG></Label>
+            </Radio>
+          </Field>
+      </Col>
+      <Col size="auto">
+          <Field>
+            <Radio value="all" checked={radioValue === 'all'} onChange={event => setRadioValue(event.target.value)} >
+              <Label><LG>Show All</LG></Label>
+            </Radio>
+          </Field>
+      </Col>
+    </Row>
+  );
+};
+
+
+
 const IncomePerInterval = () => {
 
+  const [radioValue, setRadioValue] = useState('10');
   const [data, setData] = useState(null)
+  const [showData, setShowData] = useState(null)
 
   useEffect(() => {
     fetchTotalIncomeByIntervalMetric().then( incomeMetric =>{
@@ -83,25 +117,47 @@ const IncomePerInterval = () => {
     })
   }, [])
 
+  useEffect(() => {
+    if(data){
+      const dataLen = data.length
+      switch(radioValue){
+        case '10':
+          if (dataLen > 10){ setShowData(data.slice(-10))}
+          break
+        case '20':
+          if (dataLen > 20){ setShowData(data.slice(-20))}
+          break
+        case 'all':
+          setShowData(data)
+          break
+        default:
+          setShowData(data)
+      }
+    }
+  }, [data, radioValue])
+
   return(
     <>
     {data
     ?(
-    <div style={{ height:'500px' }}>
-      <div style={{marginTop:'10%', marginBottom:'3%' }}>
-        <XXL>{"Income Received and Tax Contributed per Interval"}</XXL>
+    <div>
+      <div style={{ height:'500px' }}>
+        <div style={{marginTop:'10%', marginBottom:'3%' }}>
+          <XXL>{"Income Received and Tax Contributed per Interval"}</XXL>
+        </div>
+        <ResponsiveContainer width="95%" height="100%">
+          <ComposedChart width={500} height={400} data={showData} margin={{ top: 20, right: 20, bottom: 20, left: 20, }} >
+            <CartesianGrid stroke="#f5f5f5" />
+            <XAxis dataKey="Interval" scale="band" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="Income" barSize={20} fill="#30AABC" />
+            <Bar dataKey="Paid Tax" barSize={20} fill="#7D3D54" />
+          </ComposedChart>
+        </ResponsiveContainer>
+        <Multichoice radioValue={radioValue} setRadioValue={setRadioValue}/>
       </div>
-      <ResponsiveContainer width="100%" height="100%">
-        <ComposedChart width={500} height={400} data={data} margin={{ top: 20, right: 20, bottom: 20, left: 20, }} >
-          <CartesianGrid stroke="#f5f5f5" />
-          <XAxis dataKey="Interval" scale="band" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="Income" barSize={20} fill="#30AABC" />
-          <Bar dataKey="Paid Tax" barSize={20} fill="#7D3D54" />
-        </ComposedChart>
-      </ResponsiveContainer>
     </div>
     )
     :(
@@ -117,7 +173,6 @@ const IncomePerInterval = () => {
 const MetricsPage = () => {
 
   const allUsers = useContext(UserContext)
-          
 
   return(
     <div style={{display:'flex', alignItems:'flex-start', flexDirection:'column'}}>
